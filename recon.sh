da5209f0bd39912463c0e1a59557a95b5cf50f4a
iqrawajid <106342960+iqrawajid@users.noreply.github.com>
2022-06-14 01:18:58 +0500

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -1,4 +1,5 @@
 echo 
+figlet "Welcome"
 echo "Please enter your domain name: "
 read domain
 echo
@@ -13,16 +14,21 @@ echo ===========================
 echo
 figlet "Ip Resolution"
 echo
+#here we use forloop for ip resolution
 for ip in $(cat output.txt)
 do
-host $ip | grep "has address" | cut -d " " -f4
+host $ip | grep "has address" | cut -d " " -f4 | sort
 done
 echo 
 figlet "Brute Force"
 echo
-sudo dnsrecon -t brt -d $domain -D /home/kali/Desktop/practice/subdomains-top1million-5000.txt -f -c /home/kali/Desktop/practice/file.csv
+#I've also attached subdomain.txt file 
+#here we are using dnsrecon tool for subdomain brute host
+sudo dnsrecon -t brt -d $domain -D /home/kali/Desktop/practice/subdomains-top1million-5000.txt -f -c /home/kali/Desktop/practice/output.csv
 echo
-figlet "results"
+figlet "Results"
 echo 
-cat file.csv
+#The following command will print the entire output.csv file like cat
+awk '{ print $1 } ' output.csv | sort -u | uniq -c
 echo
+figlet "The End"
